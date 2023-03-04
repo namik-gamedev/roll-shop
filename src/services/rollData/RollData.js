9af70ec39d22669f9c5e1586ffb08702e1735fe7
Namik Guseinov <gnamik007@gmail.com>
2023-03-04 23:32:21 +0300

    3 more rolls added

--- src/services/rollData/RollData.js
+++ src/services/rollData/RollData.js
@@ -7,7 +7,6 @@ class RollData {
             imgSrc:
                'https://sushimagazin.com/upload/resize_cache/iblock/628/254_197_2/1msjm3jl2dzpabz8saak5rpzeanu8z3x.jpg?164522097118614',
             composition: 'Рис, нори, авокадо',
-            countInSet: 8,
             count: 1,
             totalPrice: 145,
          },
@@ -17,7 +16,6 @@ class RollData {
             imgSrc:
                'https://sushimagazin.com/upload/resize_cache/iblock/bd8/254_197_2/g3toj7pcg08u7agxiy2c15zk065561bq.jpg?164522097019140',
             composition: 'Рис, нори, лосось',
-            countInSet: 8,
             count: 1,
             totalPrice: 180,
          },
@@ -27,7 +25,6 @@ class RollData {
             imgSrc:
                'https://sushimagazin.com/upload/resize_cache/iblock/388/254_197_2/pmzbj0t72lzwwtx2i04vvvpyr8nng3cj.jpg?164522097118673',
             composition: 'Рис, нори, тунец',
-            countInSet: 8,
             count: 1,
             totalPrice: 175,
          },
@@ -37,9 +34,35 @@ class RollData {
             imgSrc:
                'https://sushimagazin.com/upload/resize_cache/iblock/f40/254_197_2/cjyf4f7co28y2k2dr6jkbwwafscafrdp.jpg?164522097119164',
             composition: 'Рис, нори, огурец, кунжут',
-            countInSet: 8,
             count: 1,
             totalPrice: 95,
+         },
+         {
+            title: 'Ролл с чуккой',
+            price: 95,
+            imgSrc:
+               'https://sushimagazin.com/upload/resize_cache/iblock/047/254_197_2/ok4096skihpuqm6we512vmf6og1oercf.jpg?164522097220455',
+            composition: 'Рис, нори, водоросли чукка, кунжут',
+            count: 1,
+            totalPrice: 95,
+         },
+         {
+            title: 'Острый ролл с креветкой',
+            price: 185,
+            imgSrc:
+               'https://sushimagazin.com/upload/resize_cache/iblock/46d/254_197_2/b6cdzfy8epgwpicvgcthnijen4tzci2i.jpg?164522097919525',
+            composition: 'Рис, нори, креветка, лист салата, острый соус',
+            count: 1,
+            totalPrice: 185,
+         },
+         {
+            title: 'Калифорния с тунцом',
+            price: 375,
+            imgSrc:
+               'https://sushimagazin.com/upload/resize_cache/iblock/8ea/254_197_2/hb79d230sirir0jquntktkcsfd9decsp.jpg?164542146129929',
+            composition: 'Рис, нори, тунец, авокадо, огурец, икра масаго',
+            count: 1,
+            totalPrice: 375,
          },
       ]
    }
