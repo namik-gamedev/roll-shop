fe7052806ed7502644ce82cd7e19f5615ae260fa
Namik Guseinov <gnamik007@gmail.com>
2023-03-07 21:03:30 +0300

    unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
 import './styles/App.css'
-import Cart from './components/cart/Cart'
-import Header from './components/header/Header'
-import Rolls from './components/rolls/Rolls'
 import Homepage from './pages/Homepage'
 import SuccessPurchase from './pages/SuccessPurchase'
 import { Routes, Route, Link } from 'react-router-dom'
